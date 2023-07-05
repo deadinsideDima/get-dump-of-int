2e52f11366be1fff08b3a46d913fcd012068b3ff
Dmitry Nesterenko <trapwhiteclown@gmail.com>
2023-07-05 21:54:44 +0300

    Bit!

--- BinaryRepresentation/BitsManipulation.cs
+++ BinaryRepresentation/BitsManipulation.cs
@@ -11,7 +11,44 @@ namespace BinaryRepresentation
         /// <returns>Binary memory representation of signed long integer.</returns>
         public static string GetMemoryDumpOf(long number)
         {
-            throw new NotImplementedException("You need to implement this method.");
+            string str = string.Empty;
+            bool b = false;
+            if (number < 0)
+            {
+                b = true;
+                number = ~number;
+            }
+
+            while (number != 0)
+            {
+                str = (number & 1) + str;
+                number >>= 1;
+            }
+
+            while (str.Length < 64)
+            {
+                str = '0' + str;
+            }
+
+            if (b)
+            {
+                string sTemp = string.Empty;
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (str[i] == '0')
+                    {
+                        sTemp = new string(sTemp + '1');
+                    }
+                    else
+                    {
+                        sTemp = new string(sTemp + '0');
+                    }
+                }
+
+                str = sTemp;
+            }
+
+            return str;
         }
     }
 }
